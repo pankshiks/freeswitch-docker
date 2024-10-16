57114f7346745f9494a67fb30cc5c791efdeffb2
pankshiks <shikhawebworks@gmail.com>
2024-10-16 16:48:12 +0530

    #freeswitch-sounds-en-ca-june-32000-1.0.50.tar.gz         freeswitch-sounds-en-ca-june-32000-1.0.51.tar.gz

--- fetch-sounds.sh
+++ fetch-sounds.sh
@@ -13,11 +13,106 @@ stack_sounds_get () {
     else
 	cd /tmp/sounds/md5 || { printf "error on line: %s\n" "$LINENO" && exit; }
     fi
-    #
+
     declare -a SOUNDS=(
+        #freeswitch-sounds-en-ca-june-8000-1.0.50.tar.gz
         freeswitch-sounds-en-ca-june-8000-1.0.51.tar.gz
+        #freeswitch-sounds-en-ca-june-16000-1.0.50.tar.gz
+        freeswitch-sounds-en-ca-june-16000-1.0.51.tar.gz
+        #freeswitch-sounds-en-ca-june-32000-1.0.50.tar.gz
+        freeswitch-sounds-en-ca-june-32000-1.0.51.tar.gz
+        #freeswitch-sounds-en-ca-june-48000-1.0.50.tar.gz
+        freeswitch-sounds-en-ca-june-48000-1.0.51.tar.gz
+        #freeswitch-sounds-en-us-allison-8000-1.0.0.tar.gz
+        freeswitch-sounds-en-us-allison-8000-1.0.1.tar.gz
+        #freeswitch-sounds-en-us-allison-16000-1.0.0.tar.gz
+        freeswitch-sounds-en-us-allison-16000-1.0.1.tar.gz
+        #freeswitch-sounds-en-us-allison-32000-1.0.0.tar.gz
+        freeswitch-sounds-en-us-allison-32000-1.0.1.tar.gz
+        #freeswitch-sounds-en-us-allison-48000-1.0.0.tar.gz
+        freeswitch-sounds-en-us-allison-48000-1.0.1.tar.gz
+        freeswitch-sounds-es-mx-maria-44100.tar.gz
+        #freeswitch-sounds-fr-ca-june-8000-1.0.14.tar.gz
+        #freeswitch-sounds-fr-ca-june-8000-1.0.15.tar.gz
+        #freeswitch-sounds-fr-ca-june-8000-1.0.16.tar.gz
+        #freeswitch-sounds-fr-ca-june-8000-1.0.18.tar.gz
+        #freeswitch-sounds-fr-ca-june-8000-1.0.50.tar.gz
+        freeswitch-sounds-fr-ca-june-8000-1.0.51.tar.gz
+        #freeswitch-sounds-fr-ca-june-16000-1.0.16.tar.gz
+        #freeswitch-sounds-fr-ca-june-16000-1.0.18.tar.gz
+        #freeswitch-sounds-fr-ca-june-16000-1.0.50.tar.gz
+        freeswitch-sounds-fr-ca-june-16000-1.0.51.tar.gz
+        #freeswitch-sounds-fr-ca-june-32000-1.0.14.tar.gz
+        #freeswitch-sounds-fr-ca-june-32000-1.0.15.tar.gz
+        #freeswitch-sounds-fr-ca-june-32000-1.0.16.tar.gz
+        #freeswitch-sounds-fr-ca-june-32000-1.0.18.tar.gz
+        #freeswitch-sounds-fr-ca-june-32000-1.0.50.tar.gz
+        freeswitch-sounds-fr-ca-june-32000-1.0.51.tar.gz
+        #freeswitch-sounds-fr-ca-june-48000-1.0.14.tar.gz
+        #freeswitch-sounds-fr-ca-june-48000-1.0.15.tar.gz
+        #freeswitch-sounds-fr-ca-june-48000-1.0.16.tar.gz
+        #freeswitch-sounds-fr-ca-june-48000-1.0.18.tar.gz
+        #freeswitch-sounds-fr-ca-june-48000-1.0.50.tar.gz
+        freeswitch-sounds-fr-ca-june-48000-1.0.51.tar.gz
+        freeswitch-sounds-pl-pl-espeak-8000-0.1.0.tar.gz
+        freeswitch-sounds-pl-pl-espeak-16000-0.1.0.tar.gz
+        #freeswitch-sounds-pt-BR-karina-8000-1.0.50.tar.gz
+        freeswitch-sounds-pt-BR-karina-8000-1.0.51.tar.gz
+        #freeswitch-sounds-pt-BR-karina-16000-1.0.50.tar.gz
+        freeswitch-sounds-pt-BR-karina-16000-1.0.51.tar.gz
+        #freeswitch-sounds-pt-BR-karina-32000-1.0.50.tar.gz
+        freeswitch-sounds-pt-BR-karina-32000-1.0.51.tar.gz
+        #freeswitch-sounds-pt-BR-karina-48000-1.0.50.tar.gz
+        freeswitch-sounds-pt-BR-karina-48000-1.0.51.tar.gz
+        #freeswitch-sounds-ru-RU-elena-8000-1.0.10.tar.gz
+        #freeswitch-sounds-ru-RU-elena-8000-1.0.11.tar.gz
+        #freeswitch-sounds-ru-RU-elena-8000-1.0.12.tar.gz
+        #freeswitch-sounds-ru-RU-elena-8000-1.0.13.tar.gz
+        #freeswitch-sounds-ru-RU-elena-8000-1.0.50.tar.gz
+        freeswitch-sounds-ru-RU-elena-8000-1.0.51.tar.gz
+        #freeswitch-sounds-ru-RU-elena-16000-1.0.10.tar.gz
+        #freeswitch-sounds-ru-RU-elena-16000-1.0.11.tar.gz
+        #freeswitch-sounds-ru-RU-elena-16000-1.0.12.tar.gz
+        #freeswitch-sounds-ru-RU-elena-16000-1.0.13.tar.gz
+        #freeswitch-sounds-ru-RU-elena-16000-1.0.50.tar.gz
+        freeswitch-sounds-ru-RU-elena-16000-1.0.51.tar.gz
+        #freeswitch-sounds-ru-RU-elena-32000-1.0.10.tar.gz
+        #freeswitch-sounds-ru-RU-elena-32000-1.0.11.tar.gz
+        #freeswitch-sounds-ru-RU-elena-32000-1.0.12.tar.gz
+        #freeswitch-sounds-ru-RU-elena-32000-1.0.13.tar.gz
+        #freeswitch-sounds-ru-RU-elena-32000-1.0.50.tar.gz
+        freeswitch-sounds-ru-RU-elena-32000-1.0.51.tar.gz
+        #freeswitch-sounds-ru-RU-elena-48000-1.0.10.tar.gz
+        #freeswitch-sounds-ru-RU-elena-48000-1.0.11.tar.gz
+        #freeswitch-sounds-ru-RU-elena-48000-1.0.12.tar.gz
+        #freeswitch-sounds-ru-RU-elena-48000-1.0.13.tar.gz
+        #freeswitch-sounds-ru-RU-elena-48000-1.0.50.tar.gz
+        freeswitch-sounds-ru-RU-elena-48000-1.0.51.tar.gz
+        freeswitch-sounds-ru-RU-kirill-8000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-kirill-16000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-kirill-32000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-kirill-48000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-vika-8000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-vika-16000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-vika-32000-1.0.0.tar.gz
+        freeswitch-sounds-ru-RU-vika-48000-1.0.0.tar.gz
+        #freeswitch-sounds-sv-se-jakob-8000-1.0.0.tar.gz
+        freeswitch-sounds-sv-se-jakob-8000-1.0.50.tar.gz
+        #freeswitch-sounds-sv-se-jakob-16000-1.0.0.tar.gz
+        freeswitch-sounds-sv-se-jakob-16000-1.0.50.tar.gz
+        #freeswitch-sounds-sv-se-jakob-32000-1.0.0.tar.gz
+        freeswitch-sounds-sv-se-jakob-32000-1.0.50.tar.gz
+        #freeswitch-sounds-sv-se-jakob-48000-1.0.0.tar.gz
+        freeswitch-sounds-sv-se-jakob-48000-1.0.50.tar.gz
+        freeswitch-sounds-zh-cn-sinmei-8000-1.0.51.tar.gz
+        freeswitch-sounds-zh-cn-sinmei-16000-1.0.51.tar.gz
+        freeswitch-sounds-zh-cn-sinmei-32000-1.0.51.tar.gz
+        freeswitch-sounds-zh-cn-sinmei-48000-1.0.51.tar.gz
+        freeswitch-sounds-zh-hk-sinmei-8000-1.0.51.tar.gz
+        freeswitch-sounds-zh-hk-sinmei-16000-1.0.51.tar.gz
+        freeswitch-sounds-zh-hk-sinmei-32000-1.0.51.tar.gz
+        freeswitch-sounds-zh-hk-sinmei-48000-1.0.51.tar.gz
     )
-    #
     for SOUND in "${SOUNDS[@]}"; do
         if [ ! -f /tmp/sounds/"$SOUND" ]; then
             wget -O /tmp/sounds/"$SOUND" https://files.freeswitch.org/releases/sounds/"$SOUND" || { printf "error on line: %s\n" "$LINENO" && exit; }
